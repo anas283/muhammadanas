f7c95b0a7c9e49c581dc6a201ca9fd97cd21efc8
anas283 <muhammadanasbinadam@gmail.com>
2021-03-07 09:57:23 +0800

    Redesign website

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,8 +13,21 @@ export class HomeComponent implements OnInit {
     ngOnInit(): void {
     }
 
+	scroll(el: HTMLElement) {
+		el.scrollIntoView({ behavior: 'smooth' });
+	}
+
 	openWork() {
         this.router.navigate(['/work-details'], { queryParams: { work: 'preztel-dashboard' } });
 	}
 
+    openWorkDetails(work: any) {
+		if(work == 'preztel') {
+			this.router.navigate(['/work-details'], { queryParams: { work: 'preztel-dashboard' } });
+		} else if(work == 'manage') {
+			this.router.navigate(['/work-details'], { queryParams: { work: 'manage-landing-page' } });
+		} else if(work == 'sales') {
+			this.router.navigate(['/work-details'], { queryParams: { work: 'sales-dashboard' } });
+		}
+	}
 }
